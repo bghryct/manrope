3a8ff66806f414b3270bbb4a82596a6e18672a1b
Mirko Velimirovic <bghryct@gmail.com>
2020-02-13 20:06:18 -0500

    fixed for gf vf push

--- build.sh
+++ build.sh
@@ -5,38 +5,48 @@ set -e
 
 echo "Generating Static fonts"
 
-mkdir -p ./test/ttf
-mkdir -p ./test/variable
+mkdir -p ./test/variable/
+mkdir -p ./test/variable/static/
 echo "Made font directories"
 
 # fontmake -g source/manrope.glyphs -i --round-instances -o ttf --output-dir ./test/ttf/ 
 # echo "Made ttfs"
-
+echo "Generating Statics"
+fontmake -g source/manrope.glyphs -o ttf --round-instances -a --keep-direction -i --output-dir ./test/variable/static/
+echo "Made Statics"
 
 echo "Generating VFs"
-fontmake -g source/manrope.glyphs -o variable --output-dir ./test/variable/ 
+fontmake -g source/manrope.glyphs -o variable --output-path ./test/variable/Manrope\[wght\].ttf
 echo "Made VF"
 
 cd ./test/variable/
 
-echo "autohinting"
-ttfautohint Manrope-VF.ttf Manrope-VF-AH.ttf
-#add optional arguments here to make sure that all of the hinting necessary is done correctly. Check to make sure base file hinting is set up correctly. 
-echo "autohinted"
-
-echo "removing unhinted manrope"
-rm -rf Manrope-VF.ttf
-echo "unhinted manrope removed"
-
 echo "adding dummy dsig"
-gftools fix-dsig Manrope-VF-AH.ttf --autofix
+gftools fix-dsig Manrope\[wght\].ttf --autofix
 echo "dummy dsig added"
 
+cd ..
 
-echo "fix ppem bit 3"
-gftools fix-hinting Manrope-VF-AH.ttf
-echo "bit 3 fixed"
+cd ..
 
-echo "rename fixed ttf.fix to ttf"
-mv Manrope-VF-AH.ttf.fix Manrope-VF-AH.ttf
-echo "Build COMPLETE NUCLEAR LAUNCH DETECTED"
+echo "Post processing"
+
+vfs=$(ls ./test/variable/*.ttf)
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+	gftools fix-nonhinting $vf "$vf.fix";
+	mv "$vf.fix" $vf;
+	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $vf)
+	new_file=./test/variable/$rtrip.ttx;
+	rm $vf;
+	ttx $new_file
+	rm ./test/variable/*.ttx
+done
+rm ./test/variable/*backup*.ttf
+
+echo "build complete zerg rush denied"
+
+
+
